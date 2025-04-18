f398cc17ade2e42c2c72af39ab7e54de05552252
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-18 13:32:21 +0530

    Update LambdaExpressionExample.java

--- src/main/java/com/yaksha/assignment/LambdaExpressionExample.java
+++ src/main/java/com/yaksha/assignment/LambdaExpressionExample.java
@@ -8,18 +8,9 @@ public class LambdaExpressionExample {
 
 	public static void main(String[] args) {
 		// Task 1: Create a simple lambda expression
-		Runnable printMessage = () -> System.out.println("Hello from Lambda Expression!");
-		printMessage.run(); // Execute the lambda expression
 
 		// Task 2: Lambda expression with ArrayList
-		List<String> names = new ArrayList<>();
-		names.add("John");
-		names.add("Jane");
-		names.add("Mark");
-		names.add("Paul");
 
 		// Task 3: Use lambda expression with ArrayList to filter names
-		Predicate<String> startsWithJ = name -> name.startsWith("J");
-		names.stream().filter(startsWithJ).forEach(System.out::println); // Prints names starting with "J"
 	}
 }
